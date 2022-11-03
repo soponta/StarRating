369d4dcc7bc9ab504ee16fc5e519b02422fc4e73
soponta <heber.sba@gmail.com>
2022-11-03 21:34:44 +0700

    Create LANG.js

---
+++ js/locales/LANG.js
@@ -0,0 +1,29 @@
+(function (factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && typeof module.exports === 'object') { 
+        factory(require('jquery'));
+    } else { 
+        factory(window.jQuery);
+    }
+}(function ($) {
+    "use strict";
+    $.fn.ratingLocales['<LANG>'] = {
+        defaultCaption: '{rating} Stars',
+        starCaptions: {
+            0.5: 'Half Star',
+            1: 'One Star',
+            1.5: 'One & Half Star',
+            2: 'Two Stars',
+            2.5: 'Two & Half Stars',
+            3: 'Three Stars',
+            3.5: 'Three & Half Stars',
+            4: 'Four Stars',
+            4.5: 'Four & Half Stars',
+            5: 'Five Stars'
+        },
+        clearButtonTitle: 'Clear',
+        clearCaption: 'Not Rated'
+    };
+}));
